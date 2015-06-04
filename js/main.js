30d8af1b6a173697f714da5c3a19ee4d1a11be26
Dinesh Thapa Magar <dineshtm@gmail.com>
2015-06-04 17:18:21 +0545

    Update
    New Site

---
+++ js/main.js
@@ -0,0 +1,368 @@
+
+var $=jQuery.noConflict();
+
+
+$(".margin ul").quickPagination({pageSize:"6"});
+
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+});
+
+
+//scroll spy
+
+// Cache selectors
+var lastId,
+    topMenu = $("#main-menu"),
+    topMenuHeight = topMenu.outerHeight()+15,
+    // All list items
+    menuItems = topMenu.find("a"),
+    // Anchors corresponding to menu items
+    scrollItems = menuItems.map(function(){
+      var item = $($(this).attr("href"));
+      if (item.length) { return item; }
+    });
+
+// Bind click handler to menu items
+// so we can get a fancy scroll animation
+menuItems.click(function(e){
+  var href = $(this).attr("href"),
+      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+  $('html, body').stop().animate({ 
+      scrollTop: offsetTop
+  }, 300);
+  e.preventDefault();
+});
+
+// Bind to scroll
+$(window).scroll(function(){
+   // Get container scroll position
+   var fromTop = $(this).scrollTop()+topMenuHeight;
+   
+   // Get id of current scroll item
+   var cur = scrollItems.map(function(){
+     if ($(this).offset().top < fromTop)
+       return this;
+   });
+   // Get the id of the current element
+   cur = cur[cur.length-1];
+   var id = cur && cur.length ? cur[0].id : "";
+   
+   if (lastId !== id) {
+       lastId = id;
+       // Set/remove active class
+       menuItems
+         .parent().removeClass("active")
+         .end().filter("[href=#"+id+"]").parent().addClass("active");
+   }                   
+});
+
+/*Preloader*/
+$(window).on('load', function(){
+		$('.fade-in').css({ position: 'relative', opacity: 0, top: -14 });
+		setTimeout(function(){
+			$('.loader').fadeOut(400, function(){
+				$('#preload').fadeOut(800);
+				setTimeout(function(){
+					$('.fade-in').each(function(index) {
+						$(this).delay(400*index).animate({ top : 0, opacity: 1 }, 800);
+					});
+				}, 800);
+			});
+		}, 400);
+	});
+
+
+/*wow*/
+new WOW().init();
+
+
+//BEGIN BACK TO TOP
+$(window).scroll(function(){
+	if ($(this).scrollTop() < 50) {
+		$('#totop') .fadeOut();
+	} else {
+		$('#totop') .fadeIn();
+	}
+});
+$('#totop').on('click', function(){
+	$('html, body').animate({scrollTop:0}, 'fast');
+	return false;
+});
+
+/*
+$(window).scroll(function(){
+	if ($(this).scrollTop() < 5) {
+		$('.md-universe') .css('z-index':'-1';);
+	} 
+});*/
+//END BACK TO TOP
+
+
+//Smooth scroll
+$(function() {
+	$('a[href*=#]:not([href=#])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html,body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
+});
+
+
+
+/*Universe*/
+/*
+ * requestAnimationFrame pollyfill
+ */
+if (!window.requestAnimationFrame) {
+	window.requestAnimationFrame = (window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame || function (callback) {
+		return window.setTimeout(callback, 1000 / 60);
+	});
+}
+
+
+/*!
+ * Mantis.js / jQuery / Zepto.js plugin for Constellation
+ * @version 1.2.2
+ * @author Acauã Montiel <contato@acauamontiel.com.br>
+ * @license http://acaua.mit-license.org/
+ */
+(function ($, window) {
+	/**
+	 * Makes a nice constellation on canvas
+	 * @constructor Constellation
+	 */
+	function Constellation (canvas, options) {
+		var $canvas = $(canvas),
+			context = canvas.getContext('2d'),
+			defaults = {
+				star: {
+					color: 'rgba(255, 255, 255, .5)',
+					width: 1
+				},
+				line: {
+					color: 'rgba(255, 255, 255, .5)',
+					width: 0.2
+				},
+				position: {
+					x: 0, // This value will be overwritten at startup
+					y: 0 // This value will be overwritten at startup
+				},
+				width: window.innerWidth,
+				height: window.innerHeight,
+				velocity: 0.1,
+				length: 100,
+				distance: 120,
+				radius: 150,
+				stars: []
+			},
+			config = $.extend(true, {}, defaults, options);
+
+		function Star () {
+			this.x = Math.random() * canvas.width;
+			this.y = Math.random() * canvas.height;
+
+			this.vx = (config.velocity - (Math.random() * 0.5));
+			this.vy = (config.velocity - (Math.random() * 0.5));
+
+			this.radius = Math.random() * config.star.width;
+		}
+
+		Star.prototype = {
+			create: function(){
+				context.beginPath();
+				context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+				context.fill();
+			},
+
+			animate: function(){
+				var i;
+				for (i = 0; i < config.length; i++) {
+
+					var star = config.stars[i];
+
+					if (star.y < 0 || star.y > canvas.height) {
+						star.vx = star.vx;
+						star.vy = - star.vy;
+					} else if (star.x < 0 || star.x > canvas.width) {
+						star.vx = - star.vx;
+						star.vy = star.vy;
+					}
+
+					star.x += star.vx;
+					star.y += star.vy;
+				}
+			},
+
+			line: function(){
+				var length = config.length,
+					iStar,
+					jStar,
+					i,
+					j;
+
+				for (i = 0; i < length; i++) {
+					for (j = 0; j < length; j++) {
+						iStar = config.stars[i];
+						jStar = config.stars[j];
+
+						if (
+							(iStar.x - jStar.x) < config.distance &&
+							(iStar.y - jStar.y) < config.distance &&
+							(iStar.x - jStar.x) > - config.distance &&
+							(iStar.y - jStar.y) > - config.distance
+						) {
+							if (
+								(iStar.x - config.position.x) < config.radius &&
+								(iStar.y - config.position.y) < config.radius &&
+								(iStar.x - config.position.x) > - config.radius &&
+								(iStar.y - config.position.y) > - config.radius
+							) {
+								context.beginPath();
+								context.moveTo(iStar.x, iStar.y);
+								context.lineTo(jStar.x, jStar.y);
+								context.stroke();
+								context.closePath();
+							}
+						}
+					}
+				}
+			}
+		};
+
+		this.createStars = function () {
+			var length = config.length,
+				star,
+				i;
+
+			context.clearRect(0, 0, canvas.width, canvas.height);
+
+			for (i = 0; i < length; i++) {
+				config.stars.push(new Star());
+				star = config.stars[i];
+
+				star.create();
+			}
+
+			star.line();
+			star.animate();
+		};
+
+		this.setCanvas = function () {
+			canvas.width = config.width;
+			canvas.height = config.height;
+		};
+
+		this.setContext = function () {
+			context.fillStyle = config.star.color;
+			context.strokeStyle = config.line.color;
+			context.lineWidth = config.line.width;
+		};
+
+		this.setInitialPosition = function () {
+			if (!options || !options.hasOwnProperty('position')) {
+				config.position = {
+					x: canvas.width * 0.5,
+					y: canvas.height * 0.5
+				};
+			}
+		};
+
+		this.loop = function (callback) {
+			callback();
+
+			window.requestAnimationFrame(function () {
+				this.loop(callback);
+			}.bind(this));
+		};
+
+		this.bind = function () {
+			$canvas.on('mousemove', function(e){
+				config.position.x = e.pageX - $canvas.offset().left;
+				config.position.y = e.pageY - $canvas.offset().top;
+			});
+		};
+
+		this.init = function () {
+			this.setCanvas();
+			this.setContext();
+			this.setInitialPosition();
+			this.loop(this.createStars);
+			this.bind();
+		};
+	}
+
+	$.fn.constellation = function (options) {
+		return this.each(function () {
+			var c = new Constellation(this, options);
+			c.init();
+		});
+	};
+})($, window);
+
+// Init plugin
+$('canvas').constellation({
+	line: {
+		color: 'rgba(0, 255, 0, .5)'
+	}
+});
+
+
+
+
+//email
+jQuery(function($)
+{
+	$("#contact_form").submit(function()
+	{
+		var email = $("#email").val(); // get email field value
+		var name = $("#name").val(); // get name field value
+		var msg = $("#message").val(); // get message field value
+		msg = msg + "\n\n" + name;
+		$.ajax(
+			{
+				type: "POST",
+				url: "https://mandrillapp.com/api/1.0/messages/send.json",
+				data: {
+					'key': 'Dt-Mqe6D9NwrrvaUBYIWyw',
+					'message': {
+						'from_email': email,
+						'from_name': name,
+						'headers': {
+							'Reply-To': email
+						},
+						'subject': 'Website Contact Form Submission',
+						'text': msg,
+						'to': [
+							{
+								'email': 'dineshtm@gmail.com',
+								'name': 'Dinesh Thapa Magar',
+								'type': 'to'
+							}]
+					}
+				}
+			})
+			.done(function(response) {
+				$( "div#contact_results" ).html( "<div class='alert alert-success'>Your message has been sent. Thank you!</div>" ).slideDown();
+				$("#contact_form #contact_body").slideUp();
+				$("#name").val(''); // reset field after successful submission
+				$("#email").val(''); // reset field after successful submission
+				$("#msg").val(''); // reset field after successful submission
+			})
+			.fail(function(response) {
+				$( "div#contact_results" ).html( "<div class='alert alert-danger'>Error sending message.</div>" ).slideDown();
+			});
+		return false; // prevent page refresh
+	});
+});
